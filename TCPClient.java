aaf319c656d930545e6d394e74a2cdf47f9ede70
KingJohn28 <47121147+KingJohn28@users.noreply.github.com>
2023-05-23 16:03:15 +0200

    Update TCPClient.java

--- TCPClient.java
+++ TCPClient.java
@@ -1,9 +1,15 @@
-import java.net.*;
 import java.io.*;
+import java.net.*;
 
-public class TCPClient {
-    public static void main(String[] args) throws IOException {
-    Socket clientSocket = new Socket(args[1], Integer.parseInt(args[2]));
-    //OutputStream out = clientSocket.
+class TCPClient {
+    public static void main(String[] argv) throws Exception {
+
+        for (int i = 0; i <= 1000000; i++) {
+            Socket clientSocket = new Socket(argv[1], 6789);
+            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            outToServer.writeInt(i);
+            outToServer.flush();
+            clientSocket.close();
+        }
     }
 }
