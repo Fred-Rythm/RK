9ebb42f74cf4ebb69b4c66d38edcda94813ad917
ke38neri <frederikwarmerdam@gmail.com>
2023-05-23 14:41:36 +0200

    server and client

---
+++ TCPClient.java
@@ -0,0 +1,9 @@
+import java.net.*;
+import java.io.*;
+
+public class TCPClient {
+    public static void main(String[] args) throws IOException {
+    Socket clientSocket = new Socket(args[1], Integer.parseInt(args[2]));
+    //OutputStream out = clientSocket.
+    }
+}
--- TCPServer.java
+++ TCPServer.java
@@ -1,4 +1,21 @@
+import java.net.*;
+import java.io.*;
+
+import static java.lang.System.in;
+
 public class TCPServer {
 
+
+        public static void main(String[] args) throws IOException {
+            String sentence;
+            ServerSocket welcomeSocket = new ServerSocket(2345);
+            while(true) {
+
+                Socket connectionSocket = welcomeSocket.accept();
+                DataInputStream dataIn = new DataInputStream(in);
+                int i = dataIn.readInt();
+                System.out.println(i);
+            }
+        }
 
 }
