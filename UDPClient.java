d6edbb69e807b68956d9d681bd093ed629180eaf
Fred-Rythm <87388072+Fred-Rythm@users.noreply.github.com>
2023-05-25 10:23:30 +0200

    Update UDPClient.java

--- UDPClient.java
+++ UDPClient.java
@@ -7,14 +7,15 @@ public class UDPClient {
 
     public static void main(String[] args) throws IOException {
 
-        String ipAddress = args[0];
-        final int port = Integer.parseInt(args[1]);
-        byte[]sendData;
-        DatagramSocket sock = new DatagramSocket();
+        //String ipAddress = "127.0.0.0";
+        int port = 2345;
+        byte[]sendData = new byte[1024];
+        DatagramSocket sock = new DatagramSocket(2346);
         for(int i = 0; i <=100000;i++)
         {
+            System.out.println(i);
             sendData = ByteBuffer.allocate(4).putInt(i).array();
-            DatagramPacket sendPkt = new DatagramPacket(sendData,sendData.length, InetAddress.getByName(ipAddress),port);
+            DatagramPacket sendPkt = new DatagramPacket(sendData,sendData.length, InetAddress.getByName("localhost"),port);
             sock.send(sendPkt);
         }
     }
